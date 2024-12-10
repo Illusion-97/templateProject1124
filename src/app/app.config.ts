1ef3c87c907192f12e674bb63fbf1be86f19e967
Yanis ADEKALOM <yadekalom@gmail.com>
2024-12-10 11:01:25 +0100

    Feat : cursor in interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,17 @@ import {provideRouter, withInMemoryScrolling} from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor, backendInterceptor, errorInterceptor} from '../tools/backend.interceptor';
+import {finalize} from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), // détecter les changements de valeurs des variables
     provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration: "enabled"})), // détecte les changements dans l'URL
     provideHttpClient(withInterceptors([
+      (req, next) => {
+        document.body.classList.add("cursor-wait")
+        return next(req).pipe(finalize(() => document.body.classList.remove("cursor-wait")))
+      },
       backendInterceptor,
       authInterceptor,
       errorInterceptor
